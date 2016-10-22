07fe74c151519091390f732c0319252ce5899d14
Fernando Padoan <fop@cesar.org.br>
2016-10-22 15:51:42 -0200

    Chamada à Home Activity

--- app/src/main/java/ihces/barganha/MainActivity.java
+++ app/src/main/java/ihces/barganha/MainActivity.java
@@ -1,9 +1,16 @@
 package ihces.barganha;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +19,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // Initialize the SDK before executing any other operations,
         FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
+        AppEventsLogger.activateApp(this.getApplication());
         setContentView(R.layout.activity_main);
+
+        LoginButton loginButton = (LoginButton)findViewById(R.id.login_button);
+        CallbackManager callbackManager = CallbackManager.Factory.create();
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                throw exception;
+            }
+        });
     }
 }
