c37a451d0dc9e0695c590339478a6609c5422d84
fguira <francisco.guiraldelli@gmail.com>
2016-10-22 23:07:56 -0200

    add empty activity my_advertise

---
+++ app/src/main/java/ihces/barganha/MyAdsList.java
@@ -0,0 +1,13 @@
+package ihces.barganha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyAdsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_ads_list);
+    }
+}
